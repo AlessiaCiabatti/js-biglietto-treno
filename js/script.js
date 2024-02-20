9acba339592e675dde96249eeecc47ed3af96e4a
AlessiaCiabatti <alessiaciaba@gmail.com>
2024-02-20 16:17:09 +0100

    [add] js-if-else

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 // 1 NUMERO CHILOMETRI PASSEGGERO
-let chilometri = prompt('Quanti chilemtri vuoi percorrere?');
+let chilometri = parseInt(prompt('Quanti chilemtri vuoi percorrere?'));
 console.log(chilometri);
 
 // 2 CHIEDERE ETA PASSEGGERO
-let etaPasseggero = prompt('Quanti anni hai?');
+let etaPasseggero = parseInt(prompt('Quanti anni hai?'));
 console.log(etaPasseggero);
 
 // 3 CONST EURO PER CHILOMETRO
@@ -16,6 +16,16 @@ let totale = chilometri * euroChilometro;
 let scontoMinori = totale * 0.2;
 let scontoSenior = totale * 0.4;
 
+let totaleMinori = totale - scontoMinori;
+let totaleSenior = totale - scontoSenior;
+let totaleAdulti = totale;
+
 if(etaPasseggero < 18){
+  
+}
+else if (etaPasseggero > 65){
+  
+}
+else{
   
 }
